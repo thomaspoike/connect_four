453eb3bb2603a62288b6752b39667419aca034ab
thomaspoike <thomas.kempphaug@gmail.com>
2022-06-14 23:54:00 +0200

    Red phase of red, green and refactor of TDD

---
+++ lib/connect_four.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Game logic for connect_four
+class Connect_Four
+  def board()
+  end
+end
---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require './lib/connect_four'
+
+describe 'Connect four' do
+  describe 'Board created' do
+    it 'Returns a '
